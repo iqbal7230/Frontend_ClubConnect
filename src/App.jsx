884980e3aac95e9364f13a4fad475bec78e38329
iqbal7230 <iqbalxply@gmail.com>
2025-05-29 19:20:46 +0530

    resolve build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './context/authcontext.jsx';
-import PrivateRoute from './components/PrivateRoute';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import Dashboard from './pages/Dashboard.jsx';
 import './App.css';
-import ProfilePage from './pages/ProfilePage';
-import ChatRoom from './pages/ChatRoom';
-import EventForm from './pages/EventForm';
-import LandingPost from './pages/LandingPost';
-import EventDetails from './pages/EventDetails';
-import TrendingPage from './pages/TrendingPage';
-import StudentRegisterPage from './pages/StudentRegisterPage';
-import AllEvents from './pages/AllEvents';
-import EventAttendees from './pages/EventAttendees';
-import EventSponsors from './pages/EventSponsors';
-import SponsorForm from './pages/SponsorForm'
+import ProfilePage from './pages/ProfilePage.jsx';
+import ChatRoom from './pages/ChatRoom.jsx';
+import EventForm from './pages/EventForm.jsx';
+import LandingPost from './pages/LandingPost.jsx';
+import EventDetails from './pages/EventDetails.jsx';
+import TrendingPage from './pages/TrendingPage.jsx';
+import StudentRegisterPage from './pages/StudentRegisterPage.jsx';
+import AllEvents from './pages/AllEvents.jsx';
+import EventAttendees from './pages/EventAttendees.jsx';
+import EventSponsors from './pages/EventSponsors.jsx';
+import SponsorForm from './pages/SponsorForm.jsx'
 
 
 function App() {
