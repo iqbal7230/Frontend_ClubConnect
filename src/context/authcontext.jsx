dec97f470d3fd92bb544c8a9f1d648ebec0b92b0
iqbal7230 <iqbalxply@gmail.com>
2025-05-29 20:25:43 +0530

    resolve build error

--- src/context/authcontext.jsx
+++ src/context/authcontext.jsx
@@ -2,7 +2,7 @@ import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
 const AuthContext = createContext();
-const API_URL = 'http://localhost:3000/api/v1';
+const API_URL = import.meta.env.VITE_API_BASE_URL;
 export function useAuth() {
   return useContext(AuthContext);
 }
